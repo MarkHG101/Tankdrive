932748e07a0768bfdbc8ab6d4157add7a61508bc
MarkHG101 <markhg2008@yahoo.com>
2024-01-13 13:29:10 -0500

    eee

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,8 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.XboxDrive;
+import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -21,13 +23,17 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final DriveTrain driveTrain = new DriveTrain();
+  private final CommandXboxController xboxController = new CommandXboxController(0);
+  private final XboxDrive xboxDrive = new XboxDrive(driveTrain, xboxController);
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    driveTrain.setDefaultCommand(xboxDrive);
+
     // Configure the trigger bindings
     configureBindings();
   }
@@ -48,7 +54,6 @@ public class RobotContainer {
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**
--- src/main/java/frc/robot/commands/XboxDrive.java
+++ src/main/java/frc/robot/commands/XboxDrive.java
@@ -5,11 +5,22 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.DriveTrain;
 
 public class XboxDrive extends CommandBase {
   /** Creates a new XboxDrive. */
-  public XboxDrive() {
+  DriveTrain drivetrain;
+  CommandXboxController xboxController;
+
+  public XboxDrive(DriveTrain driveTrain, CommandXboxController xboxController) {
+    this.drivetrain = driveTrain;
+    this.xboxController = xboxController;
+
+    
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drivetrain);
+
   }
 
   // Called when the command is initially scheduled.
@@ -18,7 +29,10 @@ public class XboxDrive extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    drivetrain.drive(xboxController.getRightY(),xboxController.getLeftY());
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
